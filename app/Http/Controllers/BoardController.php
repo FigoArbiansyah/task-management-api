534ca6b4a82a498ef4450fba1b5e1f7062ee2c44
FigoArbiansyah <figoarbnsyh@gmail.com>
2024-01-17 11:50:34 +0700

    new key "count" on response api boards

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponseHelper;
 use App\Models\Board;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class BoardController extends Controller
 {
     public function index() {
-        $boards = Board::all();
+        $boards_collection = Board::all();
+        $boards = $boards_collection->map(function($board) {
+            $count = Task::where('board_id', $board->id)->count();
+            return [
+                "id" => $board->id,
+                "title" => $board->title,
+                "count" => $count,
+                "created_at" => $board->created_at,
+                "updated_at" => $board->updated_at,
+            ];
+        });
         return ApiResponseHelper::success(200, 'Berhasil mendapatkan data!', $boards);
     }
 }
